db02a8712b8e84466395e77a37fb2fa9848c2240
Faquir Yuen <faquir.yuen@outlook.com>
2019-12-06 10:58:08 +0800

    20191206

--- miner_config.py
+++ miner_config.py
@@ -2,10 +2,14 @@
 more details.
 """
 
+# Private_key
+# Private_key = "13536858dedde11c020227f3d2d3b5fc983af97b8292479e30e687045b1676e3"
 # Write your generated adress here. All coins mined will go to this address
-MINER_ADDRESS = "q3nf394hjg-random-miner-address-34nf3i4nflkn3oi"
+# "9e85bbc064eb795225a6aa078f91ba5397d0d855125b3b85aab4bdf809487cc942063ca4d315ec9cd3a1a13e561e8195fc4e208ef4c7b4cb566721cbe5c8cb8b"
+MINER_ADDRESS = "noW7wGTreVIlpqoHj5G6U5fQ2FUSWzuFqrS9+AlIfMlCBjyk0xXsnNOhoT5WHoGV/E4gjvTHtMtWZyHL5cjLiw=="
 
 # Write your node url or ip. If you are running it localhost use default
+
 MINER_NODE_URL = "http://localhost:5000"
 
 # Store the url data of every other node in the network
